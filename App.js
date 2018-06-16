03827464589b8857180eee10546e2f44b7e393ba
simongaviria1 <simongaviria@ac.c4q.nyc>
2018-06-16 12:51:11 -0400

    first commit

---
+++ App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+export default class App extends React.Component {
+  constructor() {
+    this.state = {
+      userInput: ''
+    }
+  }
+  render() {
+    return (
+      
+      <View style={styles.container}>
+        <Text>hi</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
---
+++ Components/BusRoute.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {StyleSheet, Text, View} from 'react-native'
+
+class BusRoute extends React.Component{ 
+    handleClick = () => { 
+        console.log('clicked');
+
+    }
+
+    render(){
+        return (
+            <View onClick={this.handleClick}></View> 
+        )
+    }
+};
+
+export default BusRoute;
